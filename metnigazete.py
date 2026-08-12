2e820eb209216a8d97c8b59cac7dc473ba9cb263
Veli Tekin <veli.tekin42@gmail.com>
2026-08-12 07:12:18 +0300

    BUNU INDIR

--- metnigazete.py
+++ metnigazete.py
@@ -1,3 +1,10 @@
+# Metni Gazete - Resmi Gazete OCR & Markdown Dönüştürücü
+# Copyright (C) 2026 rona419
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 import os
 import sys
 import json
